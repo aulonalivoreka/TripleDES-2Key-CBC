bded5bc52e745311955192004f9da62e3b9b8915
Aulona Livoreka <116767903+aulonalivoreka@users.noreply.github.com>
2024-05-15 18:25:47 +0200

    format key

--- TripleDES.py
+++ TripleDES.py
@@ -27,3 +27,21 @@ class TripleDESApp:
         position_x = (screen_w - 800) // 2
         position_y = (screen_h - 250) // 2
         self.root_window.geometry(f"800x250+{position_x}+{position_y}")
+
+
+    
+    
+    
+    def format_key(self, key_input):
+    # Method to format the key to ensure it is 8 bytes
+    key = key_input.encode('utf-8')
+    # Check if key length is greater than 8 bytes and truncate if necessary
+    if len(key) > 8:
+        messagebox.showinfo("Info", "Key is too long. It will be truncated to 8 characters.")
+        key = key[:8]  # Truncate to 8 bytes
+    # If key length is less than 8 bytes, pad it to reach 8 bytes
+    elif len(key) < 8:
+        messagebox.showinfo("Info", "Key is too short. It will be padded to 8 characters.")
+        key = pad(key, DES.block_size, style='pkcs7')
+    return key
+
